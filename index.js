1feae298bf6b22cc5f8027642a584314963a022b
Luke Chang <iamlukechang@gmail.com>
2014-05-19 21:39:39 -0700

    added expandDir method

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var fs = require('fs');
 var rh = require('./lib/rohac.js');
 
 if (!fs.existsSync('./build')) fs.mkdirSync('./build');
-rh.expand('./example/index.html', './build/index.html');
+rh.expandDir('./example/', './build/');
